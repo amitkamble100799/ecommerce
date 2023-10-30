630e532b9d4748f1d544fb59abd74f3eadd9ffbc
amitkamble100799 <114214703+amitkamble100799@users.noreply.github.com>
2023-10-31 01:41:27 +0530

    final code

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ dotenv.config()
 connectDb()
 //esmodulefix
 const __filename=fileURLToPath(import.meta.url)
-const __dirname=path.dirname(__filename)
+
+const __dirname =path.dirname(__filename)
+
 const app=express()
 
 //midle ware
